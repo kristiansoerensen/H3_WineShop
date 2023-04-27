e4515bdfeaabca6e971d2895506e42331ec2af56
kristiansoerensen <kristian.g.soerensen@hotmail.com>
2023-04-27 19:31:25 +0200

    []

---
+++ BlazorWebassembly/Models/MultiSelect.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace BlazorWebassembly.Models
+{
+    public class MultiSelect
+    {
+        public string? value { get; set; }
+        public string? Text { get; set; }
+        public bool Selected { get; set; }
+    }
+}
